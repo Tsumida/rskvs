2fef6c359e7bd61541a8b138202f2ff8a88931e3
Tsumida <elsa110249@163.com>
2020-03-04 16:17:48 +0800

    project 1 done.

--- src/bin/kvs.rs
+++ src/bin/kvs.rs
@@ -1,3 +1,32 @@
+#[macro_use]
+extern crate clap;
+use clap::{App};
+
 fn main(){
-    
+    let yaml = load_yaml!("cli_kvs.yml"); // static checking.
+    let matc = App::from_yaml(&yaml).get_matches();
+
+    //match matc.value_of()
+    match matc.subcommand(){
+        ("set", Some(sub_set)) => {
+            //let key = sub_set.value_of("KEY").unwrap_or("");
+            //let val = sub_set.value_of("VALUE").unwrap_or("");
+            eprintln!("unimplemented");
+
+        },
+        ("get", Some(sub_get)) => {
+            //let key = sub_set.value_of("KEY").unwrap_or("");
+            //let val = sub_set.value_of("VALUE").unwrap_or("");
+            eprintln!("unimplemented");
+        },
+        ("rm", Some(sub_rm)) => {
+            //let key = sub_set.value_of("KEY").unwrap_or("");
+            //let val = sub_set.value_of("VALUE").unwrap_or("");
+            eprintln!("unimplemented");
+        },
+        _ => {
+        },
+    }
+    std::process::exit(-1);
+
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,37 +1,68 @@
-/*
-KvStore::set(&mut self, key: String, value: String)
+#![deny(missing_docs)]
+//! rskvs is a little key-value storage.
 
-Set the value of a string key to a string
-
-KvStore::get(&mut self, key: String) -> Option<String>
-
-Get the string value of the a string key. If the key does not exist, return None.
-
-KvStore::remove(&mut self, key: String)
-
-*/
+use std::collections::HashMap;
 
 
+/// A KvStoree offers method get/set/remove methods like HashMap.
+/// Additionally, KvStore provides stablizability.
 pub struct KvStore{
-    
+    sto: HashMap<String, String>,
 }
 
 impl KvStore{
+    /// Create a empty KvStore.
     pub fn new()->Self{
         KvStore{
-
+            sto:HashMap::new(),
         }
     }
 
+    /// Insert a new key-value pair or update one already exists.
+    /// # Example
+    /// ```
+    /// use rskvs::KvStore;
+    /// 
+    /// let mut kvs = KvStore::new();
+    /// kvs.set("abc".to_string(), "def".to_string());
+    /// 
+    /// ```
     pub fn set(&mut self, key:String, value:String){
-        panic!()
+        let _ = self.sto.insert(key, value);
     }
 
+    /// Get value for a given key, which is  wrapped in Option.
+    /// # Example
+    /// ```
+    /// use rskvs::KvStore;
+    /// 
+    /// let mut kvs = KvStore::new();
+    /// kvs.set("abc".to_string(), "def".to_string());
+    /// assert_eq!("def".to_string(), kvs.get("abc".to_string()).unwrap());
+    /// 
+    /// ```
     pub fn get(&mut self, key:String) -> Option<String>{
-        panic!()
+        match self.sto.get(&key){
+            None => None,
+            Some(st) => Some(st.to_string()),
+        }
     }
 
+    /// Remove the key-value pair for given key. 
+    /// It's ok to remove a pair not in the KvStore.
+    /// # Example
+    /// ```
+    /// use rskvs::KvStore;
+    /// 
+    /// let mut kvs = KvStore::new();
+    /// kvs.set("abc".to_string(), "def".to_string());
+    /// kvs.remove("abc".to_string());
+    /// kvs.remove("abc".to_string()); // double removement, ok
+    /// ```
     pub fn remove(&mut self, key:String){
-        panic!()
+        let _ = self.sto.remove(&key);
     }
 }
+
+
+
